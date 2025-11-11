0b219e23f38596b040925ecb5cf865e8d35bd538
Lucas Vicente da Silva <78667161+Lucasvicentedasilva@users.noreply.github.com>
2025-11-10 23:01:43 -0300

    Update redirect.global.ts

--- app/middleware/redirect.global.ts
+++ app/middleware/redirect.global.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware((to) => {
     // Só executa para a rota raiz
     if (to.path === '/') {
         const { userId } = useAuth()
-        
+
         // Se não estiver autenticado, redireciona para /home
         if (!userId.value) {
             return navigateTo('/home', { redirectCode: 302 })
